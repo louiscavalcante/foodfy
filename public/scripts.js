70b6f0497f8e0aacd65b0547504dad49eabe59fa
louiscavalcante <louiscavalcante@hotmail.com>
2021-10-02 09:47:08 -0400

    foodfy refactoring [DONE]

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,6 @@
-const modal = document.querySelector('.recipes-modal_overlay')
-const recipes_recipes = document.querySelectorAll('.recipes-recipe')
+//! -------------------- Recipes Click Redirect 
 const index_recipes = document.querySelectorAll('.index-recipe')
+const recipes_recipes = document.querySelectorAll('.recipes-recipe')
 
 function recipeHrefParams(recipes) {
 	for (let recipe of recipes) {
@@ -13,3 +13,20 @@ function recipeHrefParams(recipes) {
 
 recipeHrefParams(index_recipes)
 recipeHrefParams(recipes_recipes)
+
+//! -------------------- Buttons Click Hide
+const hideButtons = document.querySelectorAll('.btn')
+
+for (let btn of hideButtons) {
+	btn.addEventListener('click', function () {
+		if (btn.innerText === 'ESCONDER') {
+			btn.innerText = 'MOSTRAR'
+		} else {
+			btn.innerText = 'ESCONDER'
+		}
+
+		const index = Array.prototype.indexOf.call(hideButtons, btn)
+		const hideThis = document.getElementsByClassName('hideThis')
+		hideThis[index].classList.toggle('hide')
+	})
+}
