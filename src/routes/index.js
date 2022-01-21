396cd364bd5b2f2e8266ef386bd7c311836ebcfb
louiscavalcante <louiscavalcante@hotmail.com>
2022-01-21 06:03:18 -0400

    feat: refactoring routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const routes = express.Router()
 
 const home = require('./home.js')
-const recipes = require('./recipes.js')
-const chefs = require('./chefs.js')
+const adminRecipes = require('./recipes.js')
+const adminChefs = require('./chefs.js')
 
 routes.use(home)
-routes.use('/admin/recipes', recipes)
-routes.use('/admin/chefs', chefs)
+routes.use('/admin/recipes', adminRecipes)
+routes.use('/admin/chefs', adminChefs)
 
-// Alias //
+//! Alias
 routes.get('/admin', (req, res) => res.redirect('/admin/recipes'))
 
 routes.use(function (req, res) {
